c6343992462c7be01054ddd477907e848b36ddde
Hafijul Islam <hafijul233@gmail.com>
2021-03-19 01:04:52 +0600

    rbac controller aded

---
+++ src/Http/Controllers/Auth/PermissionsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace HishabKitab\Engine\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
+
+class PermissionsController
+{
+    protected $permissionModel;
+
+    public function __construct()
+    {
+        $this->permissionModel = Config::get('laratrust.models.permission');
+    }
+
+    public function index()
+    {
+        return View::make('engine::permissions.index', [
+            'permissions' => $this->permissionModel::simplePaginate(10),
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $permission = $this->permissionModel::findOrFail($id);
+
+        return View::make('engine::edit', [
+            'model' => $permission,
+            'type' => 'permission',
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $permission = $this->permissionModel::findOrFail($id);
+
+        $data = $request->validate([
+            'display_name' => 'nullable|string',
+            'description' => 'nullable|string',
+        ]);
+
+        $permission->update($data);
+
+        Session::flash('laratrust-success', 'Permission updated successfully');
+        return redirect(route('laratrust.permissions.index'));
+    }
+}
